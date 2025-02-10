3eddd7f663af89dd32331a3ad44533d8aa22ac11
Ahmed Elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2025-02-09 23:11:45 -0600

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -40,7 +40,6 @@ class SimpleLogisiticRegression():
     def __init__(self):
         self.w = []
         pass
-
         
     def initialize_weights(self, num_features):
         #DO NOT MODIFY THIS FUNCTION
@@ -56,12 +55,13 @@ class SimpleLogisiticRegression():
         :return: loss   
         """
         # INSERT YOUR CODE HERE
-        loss = -np.sum(y* np.log(self.sigmoid(np.dot(X,self.w))) + (1-y)*np.log(1- self.sigmoid(np.dot(X,self.w))))
+        X = np.insert(X, 0, 1, axis=1)
+        prob = self.sigmoid(np.dot(X, self.w))
+        loss = -np.sum(y * np.log(prob) + (1 - y) * np.log(1 - prob)) / X.shape[0]
 
         return loss
 
         raise Exception('Function not yet implemented!')
-
     
     def sigmoid(self, val):
 
@@ -72,8 +72,8 @@ class SimpleLogisiticRegression():
         """
         # INSERT YOUR CODE HERE
         sigmoid = 1/(1+np.exp(-val))
+        return sigmoid
         raise Exception('Function not yet implemented!')
-
 
     def gradient_ascent(self, w, X, y, lr):
         # slide 14 bottom
@@ -87,9 +87,9 @@ class SimpleLogisiticRegression():
         Update the model weights
         """
         # INSERT YOUR CODE HERE
-        raise Exception('Function not yet implemented!')
-
-
+        y_pred = self.sigmoid(np.dot(X, w))  # Compute predictions for all samples
+        gradient = np.dot(X.T, (y - y_pred)) / X.shape[0]  # Compute batch gradient
+        self.w += lr * gradient  # Update weights using learning rate
 
     def fit(self,X, y, lr=0.1, iters=100, recompute=True):
         """
@@ -105,15 +105,16 @@ class SimpleLogisiticRegression():
 
         """
         # INSERT YOUR CODE HERE
-        
+        X = np.insert(X, 0, 1, axis=1)
         if(recompute):
             #Reinitialize the model weights
+            self.w = self.initialize_weights(X.shape[1])
             pass
 
         for _ in range(iters):
             # INSERT YOUR CODE HERE
+            self.gradient_ascent(self.w, X, y, lr)
             pass
-
 
     def predict_example(self, w, x):
         """
@@ -123,11 +124,14 @@ class SimpleLogisiticRegression():
         :return: predicted label for x
         """
          # INSERT YOUR CODE HERE
+        x = np.insert(x, 0, 1)
+        mayb = self.sigmoid(np.dot(x,w))
+        if (mayb >= 0.5):
+            return 1
+        return 0
         raise Exception('Function not yet implemented!')
 
-
-
-    def compute_error(y_true, y_pred):
+    def compute_error(self, y_true, y_pred):
         """
         Computes the average error between the true labels (y_true) and the predicted labels (y_pred)
         :param y_true: true label
@@ -135,10 +139,8 @@ class SimpleLogisiticRegression():
         :return: error rate = (1/n) * sum(y_true!=y_pred)
         """
         # INSERT YOUR CODE HERE
+        return np.sum(y_true != y_pred) / len(y_pred)
         raise Exception('Function not yet implemented!')
-
-
-
 
 if __name__ == '__main__':
 
@@ -155,29 +157,72 @@ if __name__ == '__main__':
     lr =  SimpleLogisiticRegression()
     
     #Part 1) Compute Train and Test Errors for different number of iterations and learning rates
+    best_learning_rate = None
+    best_iterations = None
+    best_testing_error = None
+
     for iter in [10, 100,1000,10000]:
         for a in [0.01,0.1, 0.33]:
             #INSERT CODE HERE
-            pass
+            lr.fit(Xtrn, ytrn, a, iter)
+            ytrn_pred = [lr.predict_example(lr.w, x) for x in Xtrn]
+            ytst_pred = [lr.predict_example(lr.w, x) for x in Xtst]
+            train_error = lr.compute_error(ytrn, ytrn_pred)
+            test_error = lr.compute_error(ytst, ytst_pred)
+            print(f"Iterations: {iter}, Learning Rate: {a}, Train Error: {train_error}, Test Error: {test_error}")
+
+            if best_testing_error is None or test_error < best_testing_error:
+                best_testing_error = test_error
+                best_learning_rate = a
+                best_iterations = iter
+
 
     #Part 2) Retrain Logistic Regression on the best parameters and store the model as a pickle file
     #INSERT CODE HERE
 
+    lr.fit(Xtrn, ytrn, best_learning_rate, best_iterations)
+
+    
+
+
     # Code to store as pickle file
     netid = 'AXE210038'
-    file_pi = open('{}_model_1.obj',format(netid), 'wb')  #Use your NETID
+    file_pi = open('{}_model_1.obj'.format(netid), 'wb')  #Use your NETID
     pickle.dump(lr, file_pi)
 
 
     #Part 3) Compare your model's performance to scikit-learn's LR model's default parameters 
     #INSERT CODE HERE
+    sklearn = LogisticRegression()
+    sklearn.fit(Xtrn, ytrn)
+    ytrn_pred = sklearn.predict(Xtrn)
+    ytst_pred = sklearn.predict(Xtst)
+    train_error = lr.compute_error(ytrn, ytrn_pred)
+    test_error = lr.compute_error(ytst, ytst_pred)
+    print(f"SCIKIT Train Error: {train_error}, Test Error: {test_error}")
+
+
+
 
     #Part 4) Plot curves on train and test loss for different learning rates. Using recompute=False might help
-    for a in [0.01,0.1, 0.33]:
+    plt.figure(figsize=(10, 5))
+    for a in [0.01, 0.1, 0.33]:
+        train_losses = []
+        test_losses = []
         lr.fit(Xtrn, ytrn, lr=a, iters=1)
-        #INSERT CODE HERE
         for i in range(10):
-            lr.fit(Xtrn, ytrn, lr=a, iters=100,recompute=False)
-            #INSERT CODE HERE
-
+            lr.fit(Xtrn, ytrn, lr=a, iters=100, recompute=False)
+            train_loss = lr.compute_loss(Xtrn, ytrn)
+            test_loss = lr.compute_loss(Xtst, ytst)
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
+        
+        plt.plot(range(1, 11), train_losses, label=f'Train Loss (lr={a})')
+        plt.plot(range(1, 11), test_losses, label=f'Test Loss (lr={a})', linestyle='dashed')
+    
+    plt.xlabel('Iterations (x100)')
+    plt.ylabel('Loss')
+    plt.title('Train and Test Loss Curves for Different Learning Rates')
+    plt.legend()
+    plt.show()
 
